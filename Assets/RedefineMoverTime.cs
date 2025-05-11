9e62c161fc9aad691f6204691c6c2a85f2118cb3
Азат Ра <azat.ra.xim@mail.ru>
2025-05-11 15:38:59 +0500

    Add files via upload

---
+++ Assets/RedefineMoverTime.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+
+public class RedefineMoverTime : MonoBehaviour
+{
+    [Header("Положение объекта")]
+    [SerializeField] private Vector3 _startPosition = new Vector3(-2.5f, 0, 0);
+    [SerializeField] private Vector3 _endPosition = new Vector3(2.5f, 0, 0);
+    [Tooltip("Количество шагов движения")]
+    [SerializeField] private int _stepsCount = 10;
+    [Tooltip("Длительность движения в секундах")]
+    [SerializeField] private float _duration = 1.0f;
+
+    private IEnumerator _enumerator;
+
+    private void Start()
+    {
+        StartCoroutine(MoveObject());
+    }
+
+    private IEnumerator MoveObject()
+    {
+        Vector3 distance = _endPosition - _startPosition;
+        Vector3 stepValue = distance / _stepsCount; // Шаг перемещения
+
+        for (int step = 0; step <= _stepsCount; step++)
+        {
+            transform.position = _startPosition + stepValue * step; // Обновляем позицию
+            yield return new WaitForSeconds(_duration / _stepsCount); // Задержка между шагами
+        }
+    }
+}
