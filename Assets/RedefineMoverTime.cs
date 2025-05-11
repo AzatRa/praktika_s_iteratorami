02744cc47facfc28a8ecca1f3a774afa3db0620f
Азат Ра <azat.ra.xim@mail.ru>
2025-05-11 16:25:29 +0500

    Update RedefineMoverTime.cs

--- Assets/RedefineMoverTime.cs
+++ Assets/RedefineMoverTime.cs
@@ -3,15 +3,13 @@ using UnityEngine;
 
 public class RedefineMoverTime : MonoBehaviour
 {
-    [Header("Положение объекта")]
+    [Header("РџРѕР»РѕР¶РµРЅРёРµ РѕР±СЉРµРєС‚Р°")]
     [SerializeField] private Vector3 _startPosition = new Vector3(-2.5f, 0, 0);
     [SerializeField] private Vector3 _endPosition = new Vector3(2.5f, 0, 0);
-    [Tooltip("Количество шагов движения")]
+    [Tooltip("РљРѕР»РёС‡РµСЃС‚РІРѕ С€Р°РіРѕРІ РґРІРёР¶РµРЅРёСЏ")]
     [SerializeField] private int _stepsCount = 10;
-    [Tooltip("Длительность движения в секундах")]
+    [Tooltip("Р”Р»РёС‚РµР»СЊРЅРѕСЃС‚СЊ РґРІРёР¶РµРЅРёСЏ РІ СЃРµРєСѓРЅРґР°С…")]
     [SerializeField] private float _duration = 1.0f;
-
-    private IEnumerator _enumerator;
 
     private void Start()
     {
@@ -21,12 +19,12 @@ public class RedefineMoverTime : MonoBehaviour
     private IEnumerator MoveObject()
     {
         Vector3 distance = _endPosition - _startPosition;
-        Vector3 stepValue = distance / _stepsCount; // Шаг перемещения
+        Vector3 stepValue = distance / _stepsCount; // РЁР°Рі РїРµСЂРµРјРµС‰РµРЅРёСЏ
 
         for (int step = 0; step <= _stepsCount; step++)
         {
-            transform.position = _startPosition + stepValue * step; // Обновляем позицию
-            yield return new WaitForSeconds(_duration / _stepsCount); // Задержка между шагами
+            transform.position = _startPosition + stepValue * step; // РћР±РЅРѕРІР»СЏРµРј РїРѕР·РёС†РёСЋ
+            yield return new WaitForSecondsRealtime(_duration / _stepsCount); // Р—Р°РґРµСЂР¶РєР° РјРµР¶РґСѓ С€Р°РіР°РјРё
         }
     }
 }
